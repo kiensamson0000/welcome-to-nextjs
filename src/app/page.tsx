b78100772a5deae9bf4692b516c09da29fa4522f
kiensamson0000 <kienkh99@@gmail.com>
2025-03-29 10:49:57 +0700

    fetching data with fetch and swr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,51 @@
 "use client";
-
 import Link from "next/link";
 import style1 from "@/styles/app.module.css";
 import style2 from "@/styles/customCSS.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TableComponent from "@/components/app.table";
+// import { useEffect } from "react";
+import useSWR from "swr";
 
 export default function Home() {
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:8000/blogs",
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    }
+  );
+
+  console.log(">>> check data: ", data);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch("http://localhost:8000/blogs");
+  //     const data = await res.json();
+
+  //     console.log(">>> check res: ", data);
+  //   };
+  //   fetchData();
+  // }, []);
+
   return (
     <div>
+      <div>{data?.length}</div>
+
       <ul>
         <li className={style1["red"]}>
-          <Link href={`/youtube`}>
-            <span className={style2["red"]}>Youtube</span>
+          <Link href={"/facebook"}>
+            <span className={style2["red"]}>Facebook</span>
           </Link>
         </li>
-        <li>
-          <Link href={`/facebook`}>Facebook</Link>
+        <li style={{ margin: "20px 0" }}>
+          <Link href={"/youtube"}>Youtube</Link>
         </li>
-        <li>
-          <Link href={`/tiktok`}>Tiktok</Link>
+        <li style={{ margin: "20px 0" }}>
+          <Link href={"/tiktok"}>Tiktok</Link>
         </li>
       </ul>
       <TableComponent />
