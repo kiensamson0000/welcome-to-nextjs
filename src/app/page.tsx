d7923d3af08fe7e17b4b1aced5cd2f81c25f515b
kiensamson0000 <kienkh99@@gmail.com>
2025-03-29 22:06:16 +0700

    refactor home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,16 @@
-import Link from "next/link";
-import style1 from "@/styles/app.module.css";
-import style2 from "@/styles/customCSS.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import type { Metadata } from "next";
+import HomePage from "@/components/homepages";
 
 export const metadata: Metadata = {
-  title: "HomePage",
+  title: "Home Page",
   description: "Welcome to NextJS",
 };
 
 export default function Home() {
   return (
     <div>
-      <ul>
-        <li className={style1["red"]}>
-          <Link href={"/facebook"}>
-            <span className={style2["red"]}>Facebook</span>
-          </Link>
-        </li>
-        <li style={{ margin: "20px 0" }}>
-          <Link href={"/youtube"}>Youtube</Link>
-        </li>
-        <li style={{ margin: "20px 0" }}>
-          <Link href={"/tiktok"}>Tiktok</Link>
-        </li>
-      </ul>
+      <HomePage />
     </div>
   );
 }
